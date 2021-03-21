2e0754d99fb106f71a5e3266560d0f5466811ec2
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-21 11:32:03 -0400

    :loud_sound: add new seeders for country, state and city with informations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(CountrySeeder::class);
+        $this->call(StateSeeder::class);
+        $this->call(CitySeeder::class); 
+        $this->call(UsersSeeder::class);
     }
 }
