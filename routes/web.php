8d05964d812c61ea7bf0268cf85ea19162b87778
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-22 10:38:11 -0400

    :truck: develop

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 
 Route::resource('users', 'UserController');
