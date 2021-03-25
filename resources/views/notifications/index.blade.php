db188abb898e2bdfbab5c31ffd8e5873cbe79146
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-25 16:30:02 -0400

    :bug: success bug

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -8,9 +8,11 @@
                 <ul class="list-group">
                     @foreach($unreadNotifications as $unreadNotification)
                         <li class="list-group-item">
-                          <a href=" {{$unreadNotification->data['link']}}">
+
+                         
+                           <a href="{{$unreadNotification->data['link']}}">
                              {{$unreadNotification->data['text']}}
-                          </a>
+                          </a> 
                           <form class="pull-rigth" action="{{route('notifications.read', $unreadNotification->id)}}" method="post">
                               @method('PATCH')
                               @csrf
@@ -24,10 +26,14 @@
                 <h3>Notificaciones  Leidas</h3>
                 <ul class="list-group"> 
                     @foreach($notifications as $notification)
+
+                   
                         <li class="list-group-item">
-                            <a href=" {{$notifications->data['link']}}">
+                            @if($notification->data['link'])
+                             <a href="{{$notification->data['link']}}">
                                 {{$notification->data['text']}}
-                             </a>
+                             </a> 
+                             @endif
                         </li>
                     @endforeach
                 </ul>
@@ -35,3 +41,16 @@
         </div>
     </div>    
 @endsection
+
+{{-- 
+    
+    {"id":"07e10ba0-25db-4504-bc96-6d8e8410aadf",
+    "type":"App\\Notifications\\MassageSend",
+    "notifiable_type":"App\\User",
+    "notifiable_id":1,
+    "data":
+    {"link":"http:\/\/127.0.0.1:8000\/messages\/7",
+    "text":"Has Recibido un mensaje de test"},
+    "read_at":null,"created_at":"2021-03-23T20:28:06.000000Z",
+    "updated_at":"2021-03-23T20:28:06.000000Z"}
+--}}
